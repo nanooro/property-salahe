b830cef6e7d81fb0dc394d86e578921660e75577
skydev <notnotsky26@gmail.com.>
2025-12-06 13:24:01 +0530

    changed hero secondary btn text from contact support to Call us

--- src/components/hero-section-demo-1.tsx
+++ src/components/hero-section-demo-1.tsx
@@ -60,7 +60,7 @@ export default function HeroSectionOne() {
           Explore Now
         </button>
         <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-          Contact Support
+         Call us 
         </button>
       </motion.div>
       <motion.div
